32a4db8190d7b6c9d5f41805f4d71c0ef63b1644
AltinZili <Altin.ZILI@cpnv.ch>
2019-12-18 00:33:39 +0100

    Calendrier presque fini à rendre

--- Exo_traduction/Calendrier/index.php
+++ Exo_traduction/Calendrier/index.php
@@ -1,7 +1,7 @@
 <!--
     Projet:
     Auteur: Altin Zili
-    Date: 
+    Date:
     Version:
     Description
 -->
@@ -16,24 +16,60 @@
 </head>
 <body>
 <?php
-    $month = $_GET['month'];
-    $month += 0;
-    $year = $_GET['year'];
+    $month = $_GET['month']; //récupère dans la query string le mois choisi
+    $month += 0; //met le met sous forme d'entier pour pouvoir faire des opérations
+    $year = $_GET['year']; //récupère dans la query string le mois choisi
 
-    $dayOfTheMonth = 1;
+    //cette fonction permet, si le mois est janvier de faire en sorte que le mois d'avant soit décembre
+    function getMonthBefore($month,$year){
+        if($month  == 1){
+            $previousMonth = 12;
+        }else{
+            $previousMonth = $month-1;
+        }
+        return array($previousMonth);
+    }
+    //ici on récupère la variable du mois précédent de la function getMonthBefore
+    list($previousMonth) = getMonthBefore($month,$year);
 
-    $daysOfTheWeek = array("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su");
-    $monthsOfTheYear = array("January", "February", "March", "April", "May", "Juni", "July", "August", "September", "October", "November", "December");
 
-    strtotime("$year-$month-01");
-    echo "<div class='month'>";
-        echo "<ul>";
-            echo "<li>";
-                echo $monthsOfTheYear[$month-1]."<br>$year";
-            echo "</li>";
-        echo "</ul>";
-    echo "</div>";
+    $firstDay = strtotime("$year-$month-1"); //le premier jour du mois à afficher en secondes stocké dans une variable
+    $daysOfTheWeek = array("Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"); //tableau des jours de la semaine
+    $firstDayOfPreviousMonth = strtotime("$year-$previousMonth-1"); //le premier jour du mois précédant celui à afficher en secondes stocké dans une variable
+    $weekDayOfFirstDayOfTheMonth = date ('N', $firstDay); //donne la valeur du jour de la semaine correspondant à la variable firstDay
+    $daysOfTheMonth = 1; //donne 1 comme première valeur aux jours du mois
+    $nbOfDaysOfTheMonth = date ('t', $firstDay); // donne le nombre de jours du mois en cours
+    $lastDay = strtotime("$year-$month-$nbOfDaysOfTheMonth"); //Donne en seconde moment du dernier jour du mois en cours
+    $weekDayOfLastDayOfTheMonth = date('N', $lastDay); //donne la valeur du jour de la semaine correspondant au dernier jour du mois
 
+
+    $nbOfDaysOfThePreviousMonth = date('t', $firstDayOfPreviousMonth); //donne le nombre de jours du mois précédant celui à afficher
+
+
+    $startOfLine = $nbOfDaysOfThePreviousMonth - $weekDayOfFirstDayOfTheMonth + 2; //donne le numéro du jour commencant la première ligne du calendrier
+
+
+
+
+    //cette fonction va créer le header qui comprend le mois à afficher en toute lettre et l'année
+    function GetHeader($month,$year)
+    {
+        $monthsOfTheYear = array("","January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"); //tableau des mois de l'année
+        $res = "";
+        $res .= '<div class="month">';
+        $res .= "<ul>";
+        $res .= "<li>";
+        $res .=" $monthsOfTheYear[$month]<br>$year</li>";
+        $res .= "</ul>";
+        $res .= "</div>";
+        return $res;
+    }
+
+
+    echo GetHeader($month, $year); //appelle la fonction GetHeader
+
+
+    //permet d'afficher les jours de la semaine dans le calendrier
     echo "<ul class='weekdays'>";
         for($j = 0; $j < 7; $j++)
         {
@@ -43,46 +79,38 @@
         }
     echo "</ul>";
 
+
+    //ici sera affiché le contenu du calendrier
     echo "<ul class='days'>";
-        for($i = 0; $i < 31; $i++)
+    for($i = 1; $i <= ($nbOfDaysOfTheMonth + $weekDayOfFirstDayOfTheMonth - 1); $i++)
+    {
+
+        if($i >= $weekDayOfFirstDayOfTheMonth) //affiche les jours du mois à afficher, sinon affiche les jours du mois précédant
         {
-            echo"<li>";
-                echo "$dayOfTheMonth";
-                $dayOfTheMonth += 1;
-            echo"</li>";
+            if(($daysOfTheMonth == date('j')) && ($month == date('n')) && ($year == date('Y'))) //mettre en évidence si le date du jour est affichée dans le calendrier
+            {
+                echo"<li><span class='active'>$daysOfTheMonth</span></li>";
+            }else{
+                echo"<li>$daysOfTheMonth</li>";
+            }
+
+            $daysOfTheMonth += 1;
+        }else{
+            echo"<li><span class='otherMonths'>$startOfLine</span></li>";
+            $startOfLine += 1;
         }
+
+    }
+    //affichera les premiers jours du mois suivant celui à afficher
+    for($p = 1; $p <= 7 - $weekDayOfLastDayOfTheMonth; $p++)
+    {
+        echo"<li><span class='otherMonths'>$p</span></li>";
+    }
+
     echo "</ul>";
+
+
 ?>
-<div class="month">
-    <ul>
-        <li>August<br>2017</li>
-    </ul>
-</div>
-
-<ul class="weekdays">
-    <li>Mo</li>
-    <li>Tu</li>
-    <li>We</li>
-    <li>Th</li>
-    <li>Fr</li>
-    <li>Sa</li>
-    <li>Su</li>
-</ul>
-
-<ul class="days">
-    <li>1</li>
-    <li>2</li>
-    <li>3</li>
-    <li>4</li>
-    <li>5</li>
-    <li>6</li>
-    <li>7</li>
-    <li>8</li>
-    <li>9</li>
-    <li><span class="active">10</span></li>
-    <li>11</li>
-</ul>
-</body>
 
 
 
